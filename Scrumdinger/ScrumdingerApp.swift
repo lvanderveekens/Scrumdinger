61164929588a2ca293249bde63d7692d481184be
Luciano van der Veekens <lvanderveekens@proton.me>
2024-04-07 09:21:34 +0200

    Progress

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,10 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Luciano van der Veekens on 06/04/2024.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
